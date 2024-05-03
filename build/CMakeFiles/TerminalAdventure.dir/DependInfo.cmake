75d2348b4d091dd7e6e2a36b2fd9be60e292e6d7
ST3PAN0V <st3pnv@gmail.com>
2024-05-03 22:56:19 +0300

    Add color and drop from enemy

---
+++ build/CMakeFiles/TerminalAdventure.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/stepanov/Documents/VS_code/C++/TerminalAdventure/sources/GameLogic/GameObjects.cpp" "CMakeFiles/TerminalAdventure.dir/sources/GameLogic/GameObjects.cpp.o" "gcc" "CMakeFiles/TerminalAdventure.dir/sources/GameLogic/GameObjects.cpp.o.d"
+  "/home/stepanov/Documents/VS_code/C++/TerminalAdventure/sources/WindowAndGraphics/WindowTools.cpp" "CMakeFiles/TerminalAdventure.dir/sources/WindowAndGraphics/WindowTools.cpp.o" "gcc" "CMakeFiles/TerminalAdventure.dir/sources/WindowAndGraphics/WindowTools.cpp.o.d"
+  "/home/stepanov/Documents/VS_code/C++/TerminalAdventure/sources/main.cpp" "CMakeFiles/TerminalAdventure.dir/sources/main.cpp.o" "gcc" "CMakeFiles/TerminalAdventure.dir/sources/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
